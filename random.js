66e0e471a6c379e43663e1bfbced5a0e33e139b0
P20K <128546182+p20k@users.noreply.github.com>
2024-06-19 22:45:05 +0300

    Update random.js

--- random.js
+++ random.js
@@ -1,1 +1,1 @@
-function createRandomString(length) {   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";   let result = "";   for (let i = 0; i < length; i++) {     result += chars.charAt(Math.floor(Math.random() * chars.length));   }   return result; } createRandomString(
+function createRandomString(length) {const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";   let result = "";   for (let i = 0; i < length; i++) {     result += chars.charAt(Math.floor(Math.random() * chars.length));   }   return result; } createRandomString(
